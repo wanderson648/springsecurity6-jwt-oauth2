5daea75b583dd6334478e83b259d3cf4cb061df3
wanderson648 <testepedroca@gmail.com>
2024-08-30 20:18:30 -0300

    gera usuario admin e cria roles

---
+++ src/main/java/com/wo/springsecurity/config/AdminUserConfig.java
@@ -0,0 +1,50 @@
+package com.wo.springsecurity.config;
+
+import com.wo.springsecurity.entities.Role;
+import com.wo.springsecurity.entities.User;
+import com.wo.springsecurity.repository.RoleRepository;
+import com.wo.springsecurity.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(
+            RoleRepository roleRepository,
+            UserRepository userRepository,
+            BCryptPasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                user -> System.out.println("admin já existe"),
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(passwordEncoder.encode("123"));
+                    user.setRoles(Set.of(roleAdmin));
+                    userRepository.save(user);
+                }
+        );
+    }
+}
--- src/main/java/com/wo/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/wo/springsecurity/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,9 +34,11 @@ public class SecurityConfig {
     private RSAPrivateKey privateKey;
 
     @Bean
-    private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
