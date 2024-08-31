4c0799e49af57dea913d2a4b07b502e286c9cd50
wanderson648 <testepedroca@gmail.com>
2024-08-31 00:09:48 -0300

    cria recurso delÃete com diferentes roles

--- src/main/java/com/wo/springsecurity/controller/CreateTweetController.java
+++ src/main/java/com/wo/springsecurity/controller/CreateTweetController.java
@@ -1,14 +1,15 @@
 package com.wo.springsecurity.controller;
 
 import com.wo.springsecurity.controller.dto.CreateTweetDto;
+import com.wo.springsecurity.entities.Role;
 import com.wo.springsecurity.entities.Tweet;
 import com.wo.springsecurity.repository.TweetRepository;
 import com.wo.springsecurity.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -26,8 +27,8 @@ public class CreateTweetController {
     }
 
     @PostMapping("/tweets")
-    public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDto dto
-            , JwtAuthenticationToken token) {
+    public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDto dto,
+                                            JwtAuthenticationToken token) {
 
         var user = userRepository.findById(UUID.fromString(token.getName()));
 
@@ -38,4 +39,27 @@ public class CreateTweetController {
         tweetRepository.save(tweet);
         return ResponseEntity.ok().build();
     }
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
+                                            JwtAuthenticationToken token) {
+
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+
+        var tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new ResponseStatusException((HttpStatus.NOT_FOUND)));
+
+        var isAdmin = user.get().getRoles()
+                .stream()
+                .anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+        if(isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+            tweetRepository.deleteById(tweetId);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
